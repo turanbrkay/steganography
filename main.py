3568d24ad15fe58ec98415691014245dc4ed39ac
berkay <turanbrkay@gmail.com>
2023-12-03 23:55:52 +0300

    embedded system

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def convert_grayscale(image_path):
 def get_image(image_path):
     """Get a numpy array of an image so that one can access values[x][y]."""
     image = Image.open(image_path, "r")
-
+    print(image.size)
     width, height = image.size
     pixel_values = list(image.getdata())
     if image.mode == "RGB":
@@ -22,7 +22,8 @@ def get_image(image_path):
     else:
         print("Unknown mode: %s" % image.mode)
         return None
-    pixel_values = np.array(pixel_values).reshape((width, height, channels))
+
+    pixel_values = np.array(pixel_values).reshape((height,width , channels))
     return pixel_values
 
 
@@ -33,7 +34,8 @@ def convert_int_to_binary(value):
 def convert_ASCII_to_binary(text):
     byte_array = text.encode()
     binary_int = int.from_bytes(byte_array, "big")
-    binary_string = bin(binary_int)
+    # python added 0b for represent binary string but we removed it.
+    binary_string = bin(binary_int)[2:]
     return binary_string
 
 
@@ -45,33 +47,61 @@ def convert_binary_to_ASCII(text):
     ascii_text = binary_array.decode()
     return ascii_text
 
+def save_embedded_image(image,image_path):
+    new_image = Image.fromarray(image.astype(np.uint8))
+    new_image.save(image_path+".png")
 
-def process(image_path, text):
+def embed_text(image_path, text):
     image = get_image(image_path)
-    width, height, channels = image.shape
-
-    print(image[0])
-    print("-"*40)
-
+    height,width, channels = image.shape
 
     binary_text = convert_ASCII_to_binary(text)
-    len_text = format(len(binary_text), '011b')
+
+    length_of_text = format(len(binary_text), '011b')
     # append length of binary to beginning of binary_text
-    new_binary = len_text + binary_text
+    new_binary = length_of_text + binary_text
+    print("Number of turn height: " ,int(len(new_binary)/width)+1)
+
+    for i, row in enumerate(image):
+        for j, pixel in enumerate(row):
+            # embed pixel's 0th element
+            binary_string = convert_int_to_binary(pixel[0]) # 93 -> 101101011 (int to binary)
+            new_string = binary_string[:-1] + new_binary[0]
+            decimal_value = int(new_string, 2) #convert changed number
+            pixel[0] = decimal_value #embed
+            new_binary = new_binary[1:]
+
+            if(len(new_binary) == 0):
+                break
+        if (len(new_binary) == 0):
+            break
 
 
-
-    for i in range((len(new_binary)/width)+1):
-        for j in range(5):
+    for i in range(int(len(new_binary)/width)+1):
+        for j in range(width):
             binary_string = convert_int_to_binary(image[i][j][0])
-            print(binary_string)
-            new_string = binary_string[:-1] + "X"
+            print("old string:",image[i][j][0])
+            new_string = binary_string[:-1] + new_binary[0]
+            decimal_value = int(new_string, 2)
+            print("new string:",decimal_value)
+            new_binary = new_binary[1:]
+            if(len(new_binary)==0):
+                print("break")
+                break
+            print("-" * 40)
 
 
+
+
+
+    print("-" * 40)
+    print(image[0])
+    print(len(image[0]))
+    save_embedded_image(image, "new_image")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     string = "Torres"
     string2 = "0b10001100110010101110010011011100110000101101110011001000110111100100000010101000110111101110010011100100110010101110011"
-    process("small.jpg", "Fernando Torres")
+    embed_text("small.jpg", "Fernando Torres")
 
